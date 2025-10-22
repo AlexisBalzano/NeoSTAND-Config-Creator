208e1caad0931294795413171824b81ffeee63cd
Alexis <alexis.balzano@epfl.ch>
2025-10-22 16:23:02 +0200

    added taskkill before compile

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Building ConfigCreator with GCC 15.2.0...
+taskkill /F /IM ConfigCreator.exe 2>nul
 "C:\Users\Hawai\AppData\Local\Microsoft\WinGet\Packages\BrechtSanders.WinLibs.POSIX.UCRT_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\bin\g++.exe" -std=c++17 -I. ConfigCreator.cpp -o ConfigCreator.exe
 
 if %errorlevel% == 0 (
